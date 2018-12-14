7fe6e7b2a445ecf7ff10217223b871ac6e60f9a4
unknown <zhangjing06@RRCOA.COM>
2018-12-14 18:11:41 +0800

    src

--- src/main/java/com/test/restassured/DemoSuite.java
+++ src/main/java/com/test/restassured/DemoSuite.java
@@ -8,14 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //有业务关系的接口
 
 //RunWith以Suite.class模式运行
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
-        //按照以下类顺序执行case
-        InterfaceTest1.class,
-        InterfaceTest.class
-})
-@SpringBootApplication
-public class DemoSuite {
-
-
-}
+//@RunWith(Suite.class)
+//@Suite.SuiteClasses({
+//        //按照以下类顺序执行case
+//        InterfaceTest1.class,
+//        InterfaceTest.class
+//})
+//@SpringBootApplication
+//public class DemoSuite {
+//
+//}
--- src/main/java/com/test/restassured/Md5Test.java
+++ src/main/java/com/test/restassured/Md5Test.java
@@ -9,6 +9,6 @@ public class Md5Test {
         //测试环境 可由开发提供一个万能验证码
         //但是上线需要获取真实的验证码
         //开发配合token永不失效
-
+        System.out.println("testing");
     }
 }
